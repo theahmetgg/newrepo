de554fe09aeda0577db62d65c28675b083147637
Delta <delta@Delta-MacBook-Air.local>
2024-11-06 19:12:49 +0300

    pages-options updated

--- apps/isomorphic/src/app/api/auth/[...nextauth]/pages-options.ts
+++ apps/isomorphic/src/app/api/auth/[...nextauth]/pages-options.ts
@@ -2,6 +2,9 @@ import { routes } from '@/config/routes';
 import { PagesOptions } from 'next-auth';
 
 export const pagesOptions: Partial<PagesOptions> = {
-  signIn: routes.signIn,
-  error: routes.signIn,
+  signIn: routes.signIn,        // Giriş sayfası
+  error: routes.signIn,         // Hata sayfası
+  signOut: routes.signIn,       // Çıkış yaptıktan sonra yönlendirilmesi gereken sayfa
+  verifyRequest: routes.signIn, // E-posta doğrulama isteği için yönlendirme
+  newAccount: routes.dashboard, // Yeni hesap oluşturulduğunda yönlendirilecek sayfa
 };
