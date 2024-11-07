52acbdf82c357a0fd99032ee1421dafe5fb15cbb
Ahmet Gülmez <ahmet1998gulmez@gmail.com>
2024-11-07 13:52:01 +0300

    Update env.mjs

--- apps/isomorphic/src/env.mjs
+++ apps/isomorphic/src/env.mjs
@@ -7,11 +7,17 @@ export const env = createEnv({
    */
   server: {
     NODE_ENV: z.enum(['development', 'test', 'production']),
-    NEXTAUTH_SECRET:
-      process.env.NODE_ENV === 'production'
-        ? z.string().min(1)
-        : z.string().min(1).optional(),
-    NEXTAUTH_URL: z.string().url(),
+-    NEXTAUTH_SECRET:
+-      process.env.NODE_ENV === 'production'
+-        ? z.string().min(1)
+-        : z.string().min(1).optional(),
+-    NEXTAUTH_URL: z.preprocess(
+-      // This makes Vercel deployments not fail if you don't set NEXTAUTH_URL
+-      // Since NextAuth.js automatically uses the VERCEL_URL if present.
+-      (str) => process.env.VERCEL_URL ?? str,
+-      // VERCEL_URL doesn't include `https` so it cant be validated as a URL
+-      process.env.VERCEL ? z.string().min(1) : z.string().url()
+-    ),
 
     // email
     SMTP_HOST: z.string().optional(),
